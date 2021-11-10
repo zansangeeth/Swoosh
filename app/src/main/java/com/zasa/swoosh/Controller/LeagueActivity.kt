9afefd63622148cb74b2c70b6d798f5db59e2abc
zansangeeth <zansangeeth17@gmail.com>
2021-11-11 04:37:10 +0530

    swoosh finished

--- app/src/main/java/com/zasa/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/zasa/swoosh/Controller/LeagueActivity.kt
@@ -12,9 +12,20 @@ class LeagueActivity : BaseActivity() {
 
     var player  = Player("", "")
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER,player)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+
     }
 
     fun onMensClicked(view: View){
